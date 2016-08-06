be92b62fc808ba93df61a6137c75b7d497105f9d
IbrahimSassi <sassiibrahim12@gmail.com>
2016-08-06 22:04:05 +0100

    'AddingNotesComponent'

---
+++ src/app/containers/notes.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'notes-container',
+    styles : [`
+            .notes {
+        padding-top: 50px;
+        }
+        .creator {
+        margin-bottom: 40px; 
+        }
+
+    `],
+    template : `
+    <div class="row center-xs notes">
+      <div class="col-xs-6 creator">
+        note creator here
+      </div>
+      <div class="notes col-xs-8">
+        <div class="row between-xs">
+            notes card here
+        </div>
+      </div>
+    </div>
+    `
+})
+
+export class Notes {};
