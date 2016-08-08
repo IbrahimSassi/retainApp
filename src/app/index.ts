33167c1077edc35aceddf814fe17f16a7c2926c8
IbrahimSassi <sassiibrahim12@gmail.com>
2016-08-08 20:24:50 +0100

    'ReactiveStoreAndHelpers'

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,15 @@
-export { App } from './app';
 import * as services from './services';
-export { routes } from './routes'
+import { Store } from './store';
+
+export { App } from './app';
+export { routes } from './routes';
+
+
+
 const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
 
 
 export const providers = [
+    Store,
     ...mapValuesToArray(services)
 ];
